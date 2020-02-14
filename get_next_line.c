179eceb774fbd72c17e5d9edac67acdf7acff238
Lalli Kallio <lkallio@c2r5p1.hive.fi>
2020-02-14 18:12:57 +0200

    ?

--- get_next_line.c
+++ get_next_line.c
@@ -5,57 +5,45 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: lkallio <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/10/30 13:07:20 by lkallio           #+#    #+#             */
-/*   Updated: 2019/11/08 12:37:16 by lkallio          ###   ########.fr       */
+/*   Created: 2019/11/12 21:36:18 by lkallio           #+#    #+#             */
+/*   Updated: 2019/11/15 15:18:46 by lkallio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/uio.h>
 #include "get_next_line.h"
 
-static int	find_newline(char *portion, int i, int n)
+static int	gnl_join(char **line, char *portion)
 {
-	if (!n)
-		return (-1);
-	if (*portion == '\n' || !*portion)
-		return (i);
-	return (find_newline(portion + 1, i + 1, n - 1));
-}
+	char	*temp;
+	int		ret;
 
-static char	*gnl_join(char **buf_temp, char *portion, int n)
-{
-	char	*out;
-
-	if (!buf_temp)
-		return (ft_strdup(portion));
-	out = ft_strnjoin(*buf_temp, portion, n);
-	free(*buf_temp);
-	return (out);
+	if (!*line)
+		if (!(*line = ft_strnew(0)))
+			return (-1);
+	temp = *line;
+	ret = ft_strcjoin(line, portion, '\n', 0);
+	free(temp);
+	return (ret);
 }
 
 int			get_next_line(const int fd, char **line)
 {
 	static char		buf[HANDLE_MAX][BUFF_SIZE + 1];
-	char			*buf_temp;
 	int				ret;
-	int				newline;
 
-	if (fd < 0 || !(buf_temp = gnl_join(0, buf[fd], 0)))
+	if (!line || fd < 0 || fd > HANDLE_MAX - 1)
 		return (-1);
-	while ((ret = read(fd, buf[fd], BUFF_SIZE)) > 0)
+	*line = 0;
+	ret = BUFF_SIZE;
+	while (buf[fd][0] || (ret = read(fd, buf[fd], BUFF_SIZE)) > 0)
 	{
-		if ((newline = find_newline(buf[fd], 0, BUFF_SIZE)) > -1)
+		buf[fd][ret] = 0;
+		if ((ret = gnl_join(line, buf[fd])))
 		{
-			*line = gnl_join(&buf_temp, buf[fd], newline);
-			ft_strncpy(buf[fd], buf[fd] + newline + 1,
-					ft_strlen(buf[fd]));
-			return (*line ? 1 : -1);
+			ft_strcpy(buf[fd], ft_strchr(buf[fd], '\n') + 1);
+			return (ret);
 		}
-		if (!(buf_temp = gnl_join(&buf_temp, buf[fd], BUFF_SIZE)))
-			return (-1);
 		buf[fd][0] = 0;
 	}
-	return (ret);
+	return (ret < 0 ? ret : !!*line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: lkallio <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/30 16:14:26 by lkallio           #+#    #+#             */
-/*   Updated: 2019/11/08 12:37:38 by lkallio          ###   ########.fr       */
+/*   Updated: 2019/11/15 15:09:15 by lkallio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define GET_NEXT_LINE_H
 
 # include "libft.h"
-# define BUFF_SIZE 10
+# define BUFF_SIZE 1024
 # define HANDLE_MAX 4864
 
 int			get_next_line(const int fd, char **line);
